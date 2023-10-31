d4cbf520fe53f52eb4c45e73fd6e858157b5684a
MarvinRF <marvinvca@outlook.com>
2023-10-31 12:57:56 -0300

    Setup desenvolvimento criado

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className="m-10 w-20 rounded bg-white text-2xl font-bold uppercase">
+      Loader
+    </div>
+  );
+};
+
+export default Loader;
