e9fb477ca85a04a4f034a6590e577b8c103ddd75
MarvinRF <marvinvca@outlook.com>
2023-10-31 13:04:36 -0300

    Update Hero component with motion and canvas components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div
+      className={`absolute inset-0 top-[120px] mx-auto max-w-7xl 
+      ${styles.paddingX} flex flex-row items-start gap-5`}
+    >
+      <div className="mt-5 flex flex-col items-center justify-center"></div>
+    </div>
+  );
 };
 
 export default Hero;
