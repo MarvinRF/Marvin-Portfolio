80d58c5d6048dc12f88a590595d64739babbbf69
MarvinRF <marvinvca@outlook.com>
2023-11-01 11:10:34 -0300

    correcoes de bugs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,13 +5,12 @@ import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <section className="relatie h-screen w-full">
+    <section className="relative mx-auto h-screen w-full">
       <div
-        className={`absolute inset-0 top-[120px] mx-auto max-w-7xl 
-      ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  mx-auto max-w-7xl ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="mt-5 flex flex-col items-center justify-center">
-          <div className="h-5 w-5 rounded-full bg-[#915eff]" />
+          <div className="h-5 w-5 rounded-full bg-[#915EFF]" />
           <div className="violet-gradient h-40 w-1 sm:h-80" />
         </div>
         <div>
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
 import { Suspense, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
@@ -10,7 +11,6 @@ const Computers = ({ isMobile }) => {
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={1} />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -19,6 +19,7 @@ const Computers = ({ isMobile }) => {
         castShadow
         shadow-mapSize={1024}
       />
+      <pointLight intensity={1} />
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
@@ -33,7 +34,7 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 600px)');
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
 
     setIsMobile(mediaQuery.matches);
 
@@ -42,6 +43,7 @@ const ComputersCanvas = () => {
     };
 
     mediaQuery.addEventListener('change', handleMediaQueryChange);
+
     return () => {
       mediaQuery.removeEventListener('change', handleMediaQueryChange);
     };
@@ -51,6 +53,7 @@ const ComputersCanvas = () => {
     <Canvas
       frameloop="demand"
       shadows
+      dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
